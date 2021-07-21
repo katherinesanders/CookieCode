77a4ac711b98e8a5d630366d19bc57fc59694ad7
katherinesanders <katherinespro@gmail.com>
2021-07-21 12:32:52 -0600

    Added nav bar and screens.

---
+++ CookieCode/RateBadViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RateBadViewController.swift
+//  CookieCode
+//
+//  Created by Katherine Sanders on 7/21/21.
+//
+
+import UIKit
+
+class RateBadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
