cae16d4057f4db575ca69ac57e188800003484ac
katherinesanders <katherinespro@gmail.com>
2021-07-25 17:08:41 -0600

    Final

--- CookieCode/CalendarViewController.swift
+++ CookieCode/CalendarViewController.swift
@@ -5,17 +5,67 @@
 //  Created by Katherine Sanders on 7/22/21.
 //
 
+import Foundation
 import UIKit
+import CoreData
 
-class CalendarViewController: UIViewController {
+class CalendarViewController: UIViewController, ViewControllerDelegate {
 
+    var ratingsAndResponses : [ResponseCD] = []
+    
+    var tableViewDatasource: TableViewDataSource?
+    var tableViewDelegate: TableViewDelegate?
+    
+
+    @IBOutlet weak var table: UITableView!
+    
+        override func viewDidLoad() {
+           super.viewDidLoad()
+            getResponses()
+            self.tableViewDelegate = TableViewDelegate(withDelegate: self)
+                    self.tableViewDatasource = TableViewDataSource(withData: ratingsAndResponses)
+                    
+                    self.table.delegate = self.tableViewDelegate
+                    self.table.dataSource = self.tableViewDatasource
+
+       }
 
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getResponses()
+        print(ratingsAndResponses)
+        self.tableViewDelegate = TableViewDelegate(withDelegate: self)
+                self.tableViewDatasource = TableViewDataSource(withData: ratingsAndResponses)
+                
+                self.table.delegate = self.tableViewDelegate
+                self.table.dataSource = self.tableViewDatasource
     }
+    
+    func selectedCell(row: Int) {
+            print("Row: \(row)")
+        }
+    
+    func getResponses() {
+      if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+
+        if let coreDataResponses = try? context.fetch(ResponseCD.fetchRequest()) as? [ResponseCD] {
+                ratingsAndResponses = coreDataResponses
+            self.table.reloadData()
+        }
+      }
+    }
+       
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+              // Get the new view controller using segue.destination.
+              // Pass the selected object to the new view controller.
+              if let addVC = segue.destination as? RateOkViewController {
+                  addVC.nextVC = self;
+              }
+           
+    
+       }
+
     
 
 
--- CookieCode/ViewController.swift
+++ CookieCode/ViewController.swift
@@ -23,18 +23,7 @@ class ViewController: UIViewController {
     @IBAction func inputValueButton(_ sender: UIButton) {
         let sliderValue = Double(slider.value)
         
-        if 0...0.4 ~= sliderValue {
-            let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-            let inputView: RateBadViewController = mainStoryboard.instantiateViewController(withIdentifier: "ratebadvci") as! RateBadViewController
-
-            self.present(inputView, animated: true, completion: nil)
-        }
-        else if 0.3...0.7 ~= sliderValue {
-            test.text = "Middle"
-        }
-        else {
-            test.text = "High"
-        }
+        
     }
     
     //update tracker page
